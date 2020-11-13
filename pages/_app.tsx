1a950aaef1c6d0dfbc2b89c7a4af0b06bd81f947
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2020-11-13 15:21:03 +0100

    implements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import '@assets/main.css'
 import { FC } from 'react'
 import type { AppProps } from 'next/app'
 
-import { ManagedUIContext } from '@components/ui/context'
-
 const Noop: FC = ({ children }) => <>{children}</>
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -11,11 +9,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <ManagedUIContext>
-        <Layout pageProps={pageProps}>
-          <Component {...pageProps} />
-        </Layout>
-      </ManagedUIContext>
+      <Layout pageProps={pageProps}>
+        <Component {...pageProps} />
+      </Layout>
     </>
   )
 }
